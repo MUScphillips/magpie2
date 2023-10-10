c59b47eee9463cd3de0f2c2ed46aaff4afb93691
MUScphillips <141957841+MUScphillips@users.noreply.github.com>
2023-10-10 08:00:10 -0500

    Initial commit

---
+++ src/Rectangles.java
@@ -0,0 +1,21 @@
+//This import will bring in the Rectangle class. read https://docs.oracle.com/javase/8/docs/api/java/awt/Rectangle.html for more information
+
+pubic class Rectangles
+{
+    public static void main(String[] args)
+    {
+        //construct a rectangle object called box1 that has a top left corner at 5,10 with a width of 20 and a height of 30
+
+        //output the rectangle's width and height
+
+        //calculate and output the rectangle's area
+
+        //move the rectangle using the translate method so the top left corner is at 20,25
+
+        //construct a new rectangle object called box2 with the top left corner at 10,15 with a width of 25 and a height of 20
+
+        //find and construct the intersection of box1 and box2
+
+        //find and construct the smallest rectangle that contains both box1 and box2
+    }
+}
